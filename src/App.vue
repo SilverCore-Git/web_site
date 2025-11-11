fe231249c3f68ae2e44f8f2e320ed2b3341bc531
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-11 20:51:16 +0100

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
+import Footer from './components/Footer.vue';
 import Navbar from './components/Navbar.vue';
-import contenue_HeroSection from './components/contenue_Hero section.vue';
-import Nos_service from './components/nos_services/Nos_service.vue';
+import Router from './Router.vue';
 </script>
 
 <template>
-  <!-- <div class="w-screen h-screen overflow-x-hidden parent_Hero_scetion
-              flex flex-col justify-center items-center"></div> -->
 
   <div class="background_purple w-screen h-screen">
       <Navbar/>
-      <contenue_HeroSection />
-      <Nos_service/>
+      <Router />
+      <Footer />
   </div>
 
 </template>
