00a99bfdee328aa8db0e10df2f3e5331cc88dc9e
Nanofly <noah.morel.dev@gmail.com>
2025-11-11 15:27:35 +0100

    Add mockup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,14 @@
+<script setup lang="ts">
+import navbar_config from "";
+</script>
+
 <template>
-    <nav class="center parent_navbar">
+    <nav class="center parent_navbar ">
         <div class="center navbar">
             <div class="container">
                 <div class=" left center space-around cursor_pointer">
-                    <img src="../assets/silvercore_logo.svg" alt="">
-                    <div class="petit_espace"></div>
-                    <h3>Silvercore</h3>
+                    <img src="/assets/logo/score/silvercore_logo.svg" alt="">
+                    <h3 class="ml-[10px]">Silvercore</h3>
                 </div>
                 <div class="center">
                     <p class="Page cursor_pointer Body_XS">Accueil</p>
@@ -20,15 +23,16 @@
 </template>
 
 
-<style>
+<style scoped>
 .parent_navbar {
     height: 16%;
     width: 100%
+
 }
 .navbar {
     background-color: black;
 
-    height: 60%;
+    height: 53%;
     width: 92%;
     max-width: 1200px;
     border-radius: 7px;
