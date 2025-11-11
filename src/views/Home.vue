4185cc7ecda0505d3829c6dc4063d234a1572d16
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-11 20:59:00 +0100

    create home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script lang="ts" setup>
-import Contenue_HeroSection from './components/contenue_Hero section.vue';
-import Nos_service from './components/nos_services/Nos_service.vue';
+import Contenue_HeroSection from '../components/contenue_Hero section.vue';
+import Nos_service from '../components/nos_services/Nos_service.vue';
 
 
 const isHome = true;
