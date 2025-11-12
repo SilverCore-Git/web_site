88d57131011b2b703a69f0c089160d33f08b7469
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-12 19:18:14 +0100

    Update Hero.vue

--- src/views/Home/Hero.vue
+++ src/views/Home/Hero.vue
@@ -6,23 +6,23 @@ import SButton from '../../components/SButton.vue';
 <template>
     <section 
         class="flex justify-center items-center flex-col  pt-[13%]"
-        >
+    >
         <div 
-                class="max-w-3xl text-center flex justify-center items-center flex-col"
-            >
-            <div 
-                    class="text-[60px] font-bold mb-2 "
-                >
+            class="max-w-5xl text-center flex justify-center items-center flex-col gap-6"
+        >
+
+            <div class="text-[60px] font-bold ">
                 Des services numériques pensés pour votre confidentialité
             </div>
-            <div 
-                class="mb-16"
-            >
+
+            <div class="mb-4 max-w-lg">
                 Silvercore conçoit des outils numériques innovants, éthiques et respectueux de la vie privée.
             </div>
+
             <SButton
                 content="Nos services"
             />
+
         </div>
     </section>
 </template>
