a190a36e57b7c133bb4410c050294989e170d852
Noah Morel <noah.morel.dev@gmail.com>
2025-11-15 00:01:45 +0100

    Build Responsive design mobil for Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,7 +19,7 @@ const button_contact = () => {
 
             >
                 <div
-                    class="center justify-around cursor-pointer ml-[25px]"
+                    class="center justify-around cursor-pointer ml-[25px] my-[9.5px]"
 
                 >
                     <img 
@@ -31,24 +31,38 @@ const button_contact = () => {
                 </div>
 
                 <div 
-                    class="center my-[5px]"
+                    class="center"
 
                 >
 
                     <a 
                             v-for="Nav in navbar_config.tab"
-                            class="text-white p-[15px] text-white cursor-pointer Body_XS"
+                            class="text-white p-[15px] text-white cursor-pointer Body_XS 
+                                    hidden min-lg:flex
+                                "
                             :href="Nav.href"
                         
                     >{{ Nav.name }}</a>
 
                 </div>
-
-                <SButton
-                    :content= "navbar_btn.content"
-                    :href="navbar_btn.href"
-                    class="mr-[25px]"
-                />
+                
+                <div class="hidden min-lg:flex">
+                    <SButton
+                        class=" mr-[25px] hidden"
+                        :content= "navbar_btn.content"
+                        :href="navbar_btn.href"
+                    />
+                </div>
+                <div 
+                    class="flex flex-col justify-around h-[38px] w-[38px] mr-[25px]
+                         min-lg:hidden
+                    "
+                >
+                    <div
+                        v-for="value in 3"
+                        class=" bg-white w-full h-[5px] rounded-[4px]"
+                    ></div>
+                </div>
             </div>
 
 </template>
