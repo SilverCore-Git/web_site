5a32ca28fc2bd72a6d3c3f203a90b894bedeb2e4
Noah Morel <noah.morel.dev@gmail.com>
2025-11-22 00:53:43 +0100

    Update Hero.vue

--- src/views/Home/Hero.vue
+++ src/views/Home/Hero.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import SButton from '../../components/SButton.vue';
-
+import hero_config from "../../config/hero_config.json"
 </script>
 
 <template>
-    <section 
-        class="flex justify-center items-center flex-col  pt-[13%]"
-    >
-        <div 
+    <section
+        class="flex justify-center items-center flex-col pt-[15em]
+
+    ">
+    <!-- pt-[13%] -->
+        <div
             class="max-w-5xl text-center flex justify-center items-center flex-col gap-6"
         >
 
-            <div class="text-[60px] font-bold ">
-                Des services numériques pensés pour votre confidentialité
+            <div class="font-bold text-[30px]
+                md:text-[48px]
+                xl:text-[60px]
+            ">
+                {{ hero_config.title }}
+                <!-- text-[60px] -->
             </div>
 
-            <div class="mb-4 max-w-lg">
-                Silvercore conçoit des outils numériques innovants, éthiques et respectueux de la vie privée.
+            <div class="mb-4 max-w-lg text-[16px]
+                xl:text-[18px]
+            ">
+                {{ hero_config.description }}
             </div>
 
             <SButton
